39b2e214a7ea06f3e8101656482b0894ae6b004d
oscarfloress <oscar.flores@alumno.udg.mx>
2015-03-30 23:20:30 -0600

    Clase MySql con metodos para abrir y cerrar la conexion con la base de datos

---
+++ Practica_4_Base_de_datos/MySql.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace Practica_4_Base_de_datos
+{
+    public class MySql
+    {
+        protected MySqlConnection myConnection;
+
+        public MySql()
+        {
+        }
+
+        protected void abrirConexion()
+        {
+            string connectionString =
+            "Server=localhost;" +
+            "Database=agenda;" +
+            "User ID=root;" +
+            "Password=root;" +
+            "Pooling=false;";
+            this.myConnection = new MySqlConnection(connectionString);
+            this.myConnection.Open();
+        }
+        protected void cerrarConexion()
+        {
+            this.myConnection.Close();
+            this.myConnection = null;
+        }
+    }
+}
