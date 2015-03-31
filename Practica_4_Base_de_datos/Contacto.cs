9d68cbfb51530821d40dd6dfa44b5c953f451b8d
oscarfloress <oscar.flores@alumno.udg.mx>
2015-03-30 23:34:36 -0600

    Clase Contacto que hereda de la clase MySql Metodo mostrarTodos para mostrar todos los datos de la tabla agenda.contacto Metodo consultaId recibe el id y regresa la cadena consulta, la cual extrae si se puede los datos del contacto con el id que recibe Metodo insertarRegistroNuevo recibe el nombre y el telefono y los agrega a la tabla Metodo editarRegistro recibe id, nombre y telefono y los actualiza en la tabla Metodo eliminarRegistroPorId recibe el id y borra el registro de la tabla Metodo ejecutarComando ejecuta las intrucciones que afectan a la tabla como INSERT, UPDATE Y DELETE Metodo querySelect regresa la cadena con la instruccion de mostrar todos los registros de la tabla

---
+++ Practica_4_Base_de_datos/Contacto.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace Practica_4_Base_de_datos
+{
+    public class Contacto : MySql
+    {
+        public Contacto()
+        {
+        }
+
+        public void mostrarTodos()
+        {
+            this.abrirConexion();
+            MySqlCommand myCommand = new MySqlCommand(this.querySelect(),
+            myConnection);
+            MySqlDataReader myReader = myCommand.ExecuteReader();
+            if (!myReader.HasRows)
+                Console.WriteLine("La base de datos esta vacia");
+            while (myReader.Read())
+            {
+                string id = myReader["id"].ToString();
+                string nombre = myReader["nombre"].ToString();
+                string telefono = myReader["telefono"].ToString();
+                Console.WriteLine("\nId: " + id +
+                "\nNombre: " + nombre +
+                "\nTelefono: " + telefono);
+            }
+
+            myReader.Close();
+            myReader = null;
+            myCommand.Dispose();
+            myCommand = null;
+            this.cerrarConexion();
+        }
+
+        public string consultaId(string id)
+        {
+            string consulta = "";
+            this.abrirConexion();
+            string query = "Select * From contacto Where id = ?id";
+            MySqlCommand myCommand = new MySqlCommand(query, myConnection);
+            myCommand.Parameters.AddWithValue("?id", id);
+            MySqlDataReader myReader = myCommand.ExecuteReader();
+            if (myReader.Read())
+            {
+                consulta = myReader["id"].ToString() +
+                    myReader["nombre"].ToString() +
+                    myReader["telefono"].ToString();
+            }
+            return consulta;
+        }
+
+        public void insertarRegistroNuevo(string nombre, string telefono)
+        {
+            this.abrirConexion();
+            string sql = "INSERT INTO `contacto` (`nombre`, `telefono`) VALUES ('" + nombre + "', '" + telefono + "')";
+            this.ejecutarComando(sql);
+            this.cerrarConexion();
+        }
+
+        public void editarRegistro(string id, string nombre, string telefono)
+        {
+            this.abrirConexion();
+            string sql = "UPDATE `contacto` SET `nombre`='" + nombre + "',`telefono`='" + telefono + "'  WHERE (`id`='" + id + "')";
+            this.ejecutarComando(sql);
+            this.cerrarConexion();
+        }
+
+        public void eliminarRegistroPorId(string id)
+        {
+            this.abrirConexion();
+            string sql = "DELETE FROM `contacto` WHERE (`id`='" + id + "')";
+            this.ejecutarComando(sql);
+            this.cerrarConexion();
+        }
+
+        private int ejecutarComando(string sql)
+        {
+            MySqlCommand myCommand = new MySqlCommand(sql, this.myConnection);
+            int afectadas = myCommand.ExecuteNonQuery();
+            myCommand.Dispose();
+            myCommand = null;
+            return afectadas;
+        }
+
+        private string querySelect()
+        {
+            return "SELECT * " +
+            "FROM contacto";
+        }
+    }
+}
