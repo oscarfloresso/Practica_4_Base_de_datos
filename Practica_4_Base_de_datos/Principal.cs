d0c1f6859ebae719143c78728c9a45dd75235a21
oscarfloress <oscar.flores@alumno.udg.mx>
2015-03-30 23:41:03 -0600

    Se agrega un objeto de la clase Contacto para llamar a sus metodos Case 1 muestra todos los contactos Case 2 pide los datos del contacto y los inserta en la tabla Case 3 pide el id y si existe pide los datos y los actualiza Case 4 pide el id y si existe borra el contacto

--- Practica_4_Base_de_datos/Principal.cs
+++ Practica_4_Base_de_datos/Principal.cs
@@ -12,7 +12,9 @@ namespace Practica_4_Base_de_datos
 
         public void menu()
         {
+            Contacto contacto = new Contacto();
             int opcion;
+            string id, nombre, telefono;
             Console.Write("1.- Ver todos los contactos"+
                 "\n2.- Agregar contacto"+
                 "\n3.- Editar contacto"+
@@ -24,13 +26,36 @@ namespace Practica_4_Base_de_datos
                 opcion = int.Parse(Console.ReadLine());
                 switch (opcion)
                 {
-                    case 1: 
+                    case 1: contacto.mostrarTodos();
                         break;
-                    case 2: 
+                    case 2: Console.Write("Dame el nombre: ");
+                        nombre = Console.ReadLine();
+                        Console.Write("Dame el telefono: ");
+                        telefono = Console.ReadLine();
+                        contacto.insertarRegistroNuevo(nombre, telefono);
                         break;
-                    case 3: 
+                    case 3: Console.Write("Dame el id: ");
+                        id = Console.ReadLine();
+                        if (contacto.consultaId(id).Length > 0)
+                        {
+                            Console.Write("Dame el nombre: ");
+                            nombre = Console.ReadLine();
+                            Console.Write("Dame el telefono: ");
+                            telefono = Console.ReadLine();
+                            contacto.editarRegistro(id, nombre, telefono);
+                        }
+                        else
+                            Console.WriteLine("El id no existe");
                         break;
-                    case 4: 
+                    case 4: Console.Write("Dame el id: ");
+                        id = Console.ReadLine();
+                        if (contacto.consultaId(id).Length > 0)
+                        {
+                            Console.WriteLine("Contacto eliminado");
+                            contacto.eliminarRegistroPorId(id);
+                        }
+                        else
+                            Console.WriteLine("El id no existe");
                         break;
                     case 5: Console.WriteLine("Bye");
                         salir = true;
